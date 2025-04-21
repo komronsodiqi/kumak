f797e864084b3889568556baea7cb261f30d2f41
Rustam <rusik5454sss@gmail.com>
2025-04-21 22:24:12 +0500

    kumak settings

--- kumak_backEnd/kumak/settings.py
+++ kumak_backEnd/kumak/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&idn&dzflcv&en*ot$cwn#w&=uif2#t7vl-sp4&)7_8glfkuns
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework_simplejwt',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -48,6 +51,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'kumak.urls'
