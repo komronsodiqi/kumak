e37015d32cb7e630a5cd1fc36cb36cc0fdceeed3
komronsodiqi <ksodikov@icloud.comm>
2025-04-22 12:14:36 +0500

    fixed bugs

--- kumak_backEnd/category/serializers.py
+++ kumak_backEnd/category/serializers.py
@@ -1,62 +1,57 @@
 from rest_framework import serializers
-from category.moodels import Category, Product
+from category.models import Category, Product
 
 class CategorySerializer(serializers.Serializer):
+   id = serializers.IntegerField(read_only=True)
    name = serializers.CharField()
    description = serializers.CharField()
    
    def create(self, validated_data):
        instance = Category(
-           title=validated_data.get('name'),
-           description=validated_data.get('description'))
-       instance.save()
-   
-   def update(self, instance, validated_data):
-       instance.name = validated_data.get("name")
-       instance.save()
-       return validated_data
-
-class CategorySerializer2(serializers.ModelSerializer):
-   id = serializers.IntegerField(required=True)
-   name = serializers.CharField()
-   description = serializers.IntegerField()
-   
-   class Meta:
-       model = Category
-       fields = ("id", "name", "description", "created_at")
-
-class ProductSerializer(serializers.Serializer):
-   id = serializers.IntegerField()
-   name = serializers.CharField()
-   description = serializers.CharField()
-   rating = serializers.CharField()
-   price = serializers.FloatField()
-   
-   def create(self, validated_data):
-       instance = Product(
            name=validated_data.get('name'),
-           description=validated_data.get('description'),
-           ratting=validated_data.get('rating'),
-           price=validated_data.get('price'),
-           image_url=validated_data.get('image_url')
-        )
-       instance.save()
+           description=validated_data.get('description'))
        instance.save()
        return instance
    
    def update(self, instance, validated_data):
        instance.name = validated_data.get("name")
+       instance.description = validated_data.get("description", instance.description)
        instance.save()
-       return True
+       return instance
 
-class ProductSerializer2(serializers.ModelSerializer):
-   id = serializers.IntegerField()
-   name = serializers.EmailField()
+class CategorySerializer2(serializers.ModelSerializer):
+   class Meta:
+       model = Category
+       fields = ("id", "name", "description")
+
+class ProductSerializer(serializers.Serializer):
+   id = serializers.IntegerField(read_only=True)
+   name = serializers.CharField()
    description = serializers.CharField()
    rating = serializers.CharField()
-   image_url = serializers.CharField(max_length=15)
-   price = serializers.CharField()
+   price = serializers.FloatField()
+   image_url = serializers.CharField(required=False)
+   
+   def create(self, validated_data):
+       instance = Product(
+           name=validated_data.get('name'),
+           description=validated_data.get('description'),
+           rating=validated_data.get('rating'),
+           price=validated_data.get('price'),
+           image_url=validated_data.get('image_url')
+        )
+       instance.save()
+       return instance
+   
+   def update(self, instance, validated_data):
+       instance.name = validated_data.get("name", instance.name)
+       instance.description = validated_data.get("description", instance.description)
+       instance.save()
+       return instance
+
+class ProductSerializer2(serializers.ModelSerializer):
+   image_url = serializers.CharField(max_length=1500)
    
    class Meta:
        model = Product
-       fields = ("id", "name", "description" "rating", "image_url", "price", "categoryId")
+       fields = ("id", "name", "description", "rating", "image_url", "price", "categoryId")
